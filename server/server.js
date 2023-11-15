e686698901c233407e70fe2583401564cb12bd73
Kenny Koch <kennykoch47@gmail.com>
2023-11-15 11:05:51 -0500

    adding express server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello from server!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
