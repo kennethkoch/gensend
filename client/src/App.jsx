55ab8076be944d99bfa34fa20c41baeff4562708
Kenny Koch <kennykoch47@gmail.com>
2023-12-12 09:19:26 -0500

    add email generation interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
 import HomePage from './HomePage'
+import EmailGenInterface from './EmailGenInterface'
 
 function App() {
 
   return (
     <>
       <HomePage />
+      <EmailGenInterface />
     </>
   )
 }
---
+++ client/src/EmailGenInterface.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+
+const EmailGenInterface = () => {
+    return (<div>
+        <p>email gen interface here</p>
+    </div>)
+}
+
+export default EmailGenInterface
