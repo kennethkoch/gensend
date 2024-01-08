cbf7e3381bd49023e4f8e758f43814a3544333b7
Kenny Koch <kennykoch47@gmail.com>
2024-01-08 14:36:51 -0500

    get dev api key from env

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import path from 'path';
+import OpenAI from 'openai';
+
+
+dotenv.config();
 
 const app = express();
 
@@ -11,13 +16,22 @@ const corsOptions = {
     credentials: true, // Allow cookies, authorization headers, etc.
 };
 
-// app.use(cors())
 app.use(bodyParser.json())
 app.use(cors(corsOptions));
+
+
 const port = process.env.PORT || 3000;
+const apiKey = process.env.API_KEY;
 
 const buildPath = path.resolve('../client', 'dist')
 app.use(express.static(buildPath));
+
+// const configuration = new Configuration({
+//     apiKey: apiKey,
+// });
+const openai = new OpenAI({
+    apiKey: apiKey,
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(buildPath, 'index.html'));
@@ -46,18 +60,20 @@ app.post('/api/generate', async (req, res) => {
         emojiMode: req.body.emojiMode,
     }
     try {
-        // const completion = await openai.createChatCompletion({
-        //     model: "gpt-3.5-turbo",
-        //     messages: [{
-        //         role: "user",
-        //         content: `please write an email from ${openaiRequest.senderName} to ${openaiRequest.recipientName} about 
-        //     ${openaiRequest.subject}. On a scale of 0 to 10, where 0 is extremely 
-        //     casual, 5 is somewhat formal, and 10 is extremely formal, please write this email with a 
-        //     formality of ${openaiRequest.sliderValue}. Do not include the subject line in the generated email.`
-        //     }],
-        // })
+        const completion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: "user",
+                content: 'please say hello'
+                //     content: `please write an email from ${openaiRequest.senderName} to ${openaiRequest.recipientName} about 
+                // ${openaiRequest.subject}. On a scale of 0 to 10, where 0 is extremely 
+                // casual, 5 is somewhat formal, and 10 is extremely formal, please write this email with a 
+                // formality of ${openaiRequest.sliderValue}. Do not include the subject line in the generated email.`
+            }],
+        })
         console.log(openaiRequest)
-        res.send(openaiRequest)
+
+        console.log(completion)
         // res.send(completion.data.choices[0].message.content);
     } catch (err) {
         console.log(err);
