026c38b608b95b36338871ecf98967614f249976
Kenny Koch <kennykoch47@gmail.com>
2024-02-21 18:04:17 -0500

    change navbar layout, light/dark mode toggle

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import {
     Box,
     Flex,
@@ -6,10 +6,6 @@ import {
     Spacer,
     Button,
     useColorMode,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
     IconButton,
     Text,
     ButtonGroup,
@@ -18,27 +14,13 @@ import { MoonIcon, SunIcon, HamburgerIcon } from '@chakra-ui/icons';
 
 const Navbar: React.FC = () => {
     const { colorMode, toggleColorMode } = useColorMode();
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [currentUserName, setCurrentUserName] = useState('');
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const handleLogin = () => {
-        setLoggedIn(true);
-        setCurrentUserName('test user')
-    }
-
-    const handleAccountToggle = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
-
-    const handleLogout = () => {
-        setLoggedIn(false);
-    }
 
     return (
+
         <Flex
             alignItems='center'
             width='100%'
+            // flexGrow={1}
             top={0}
             p={4}
             zIndex={100}
@@ -47,48 +29,29 @@ const Navbar: React.FC = () => {
             mb={10}
             bg={colorMode === 'light' ? 'gray.300' : 'black'}
         >
-            <Flex align="center" mr={5}>
+            <Flex align="center" >
                 <Heading color={colorMode === 'light' ? 'teal.600' : 'teal.300'}>GenSend</Heading>
             </Flex>
-            {loggedIn ? (
-                <>
-                    <Spacer />
-                    <Menu>
-                        <Text mr={3} color='gray' fontSize='lg'>{`Welcome back, ${currentUserName}!`}</Text>
-                        <MenuButton mr={2} as={IconButton} icon={<HamburgerIcon />} onClick={handleAccountToggle} />
-                        <MenuList>
-                            <MenuItem>My Account</MenuItem>
-                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                        </MenuList>
-                    </Menu>
+            <Spacer />
+            {colorMode === 'light' ? (
+                <Text as='i' color='teal.600' mr={2}>
+                    <Text as="span" fontWeight="bold">Light</Text>/Dark
+                </Text>
 
-                </>
             ) : (
-                <>
+                <Text as='i' color='teal.300' mr={2}>
+                    Light/<Text as="span" fontWeight="bold">Dark</Text>
+                </Text>
 
-                    <Spacer />
-                    <Spacer />
-                    <ButtonGroup>
-
-                        <Button colorScheme="teal" onClick={handleLogin}>
-                            Login
-                        </Button>
-                        <Button colorScheme="teal" mr={2}>
-                            Sign Up
-                        </Button>
-
-                    </ButtonGroup>
-
-                </>
             )}
 
-            <Box display='flex'>
+            <Flex>
                 <IconButton
                     aria-label="Toggle dark mode"
                     icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
                     onClick={toggleColorMode}
                 />
-            </Box>
+            </Flex>
         </Flex >
     );
 };
