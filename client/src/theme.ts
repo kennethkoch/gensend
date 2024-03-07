fd7d5534fe2754ff3de4c3602626334671025867
Kenny Koch <kennykoch47@gmail.com>
2024-03-07 13:31:26 -0500

    update styles

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,18 +1,6 @@
 import { extendTheme } from '@chakra-ui/react'
 
-// 2. Add your color mode config
-// const config = {
-//     initialColorMode: 'dark',
-//     useSystemColorMode: false,
-// }
 
-// const components = {
-//     Textarea: {
-//         focusBorderColor: 'red.500',
-//     }
-// }
-
-// 3. extend the theme
 const theme = extendTheme({
     config: {
         initialColorMode: 'dark',
